b444821c3413a2dfd49787cd8c4ef92a8c181738
dmuktesh10 <dmuktesh10@gmail.com>
2017-07-17 14:04:55 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+
+$(function(){
+    $('.main-div').css({ height: $(window).innerHeight() });
+});
+$(function(){
+  $('.first').typed({
+    strings: ["Hi I am Muktesh","I am the man with plan"],
+    typeSpeed: 50
+  });
+});
